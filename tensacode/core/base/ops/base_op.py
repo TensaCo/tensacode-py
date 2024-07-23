3a34374a2dd3b8a96c037a039c51d445f89eb4ef
Jacob Valdez <jacobfv123@gmail.com>
2024-07-22 21:40:18 -0500

    save

--- tensacode/core/base/ops/base_op.py
+++ tensacode/core/base/ops/base_op.py
@@ -6,7 +6,12 @@ from typing import ClassVar
 from pydantic import BaseModel
 
 from tensacode.core.base.base_engine import BaseEngine
-from tensacode.internal.protocols.latent import LatentType
+from tensacode.internal.protocols.latent import (
+    LatentType,
+    are_latent_subtypes,
+    latent_type_subtype_distance,
+)
+from tensacode.internal.utils.misc import inheritance_distance
 
 
 class BaseOp(BaseModel, ABC):
@@ -54,11 +59,34 @@ class BaseOp(BaseModel, ABC):
     def handler_match_score(
         self,
         *args,
-        latent_type: LatentType,
-        operator_type: type,
-        operator_name: str,
         engine_type: BaseEngine,
+        latent_type: LatentType,
+        operator_type: type | None = None,
+        operator_name: str | None = None,
         **kwargs,
     ) -> int:
-        # TODO: implement this
-        return 0
+        """
+        Calculate a score for how well this op matches the given operator and engine.
+
+        Higher is better. Use -inf for no match.
+        """
+        if not issubclass(engine_type, self.engine_type):
+            return -float("inf")
+
+        if not are_latent_subtypes(sub=latent_type, parent=self.latent_type):
+            return -float("inf")
+
+        if operator_name is not None:
+            if operator_name != self.op_name:
+                return -float("inf")
+        elif operator_type is not None:
+            if not issubclass(operator_type, type(self)):
+                return -float("inf")
+        else:
+            raise ValueError("Either operator_name or operator_type must be provided")
+
+        dist = 0
+
+        # no distance calculations in the base class. it's simple down here
+
+        return dist
